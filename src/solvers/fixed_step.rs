921c9d3164b57a095a11ccb92880f96628aa2e3f
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-07-29 09:34:43 -0400

    include license in all files

--- src/solvers/fixed_step.rs
+++ src/solvers/fixed_step.rs
@@ -1,3 +1,8 @@
+// Copyright: Jonas Pleyer
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::concepts::steppers::*;
 
 use std::ops::{Add,Mul};
