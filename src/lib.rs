5dc8980860f21d0790504553cc5827e7be95652c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-08-19 17:39:07 +0200

    python bindings: removed
    decided to create a new crate which is
    specialized on these python bindings. This crate will continue as a pure
    rust crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+/// ode_solvers is a collection of different solving techniques for
+/// general ODEs. The solvers are written with generics such that they can be applied
+/// in most scenarios. Python bindings are provided with [ode_solvers_python_bindings].
+
 pub mod concepts;
-pub mod python_bindings;
 pub mod solvers;
