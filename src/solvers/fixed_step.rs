b0ff48b5466f0fed82daa6582fcdf3d72b69bd44
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-08-23 09:45:17 +0200

    solvers: testing: add first tests
    some tests still fail. A redesign of the called function will probably
    need to happen.

--- src/solvers/fixed_step.rs
+++ src/solvers/fixed_step.rs
@@ -112,3 +112,149 @@ impl Stepper for RK4 {
         Ok(())
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::super::super::concepts::errors::CalcError;
+
+    fn rhs_vec<F>(x: &Vec<F>, dx: &mut Vec<F>, t: &F, p: &F) -> Result<(), CalcError>
+    where
+        F: Copy + Add<Output=F> + Add<F,Output=F> + AddAssign + Mul<F,Output=F> + Div<F,Output=F> + Neg<Output=F> + From<i8> + std::panic::RefUnwindSafe,
+    {
+        for (xi, dxi) in x.into_iter().zip(dx.into_iter()) {
+            *dxi = - *p * *xi * *t;
+        }
+        Ok(())
+    }
+
+
+    fn rhs_add<I, F>(x: &I, dx: &mut I, t: &F, p: &F) -> Result<(), CalcError>
+    where
+        I: AddAssign + Copy + Mul<F,Output=I> + Mul<F,Output=I> + std::ops::Neg<Output=I> + std::panic::RefUnwindSafe,
+        F: Add<F,Output=F> + Sub<F,Output=F> + Mul<F,Output=F> + Div<F,Output=F> + AddAssign + SubAssign + Neg<Output=F> + Copy + From<i8> + std::panic::RefUnwindSafe + Mul<I,Output=I>,
+    {
+        *dx = - *p * *x * *t;
+        Ok(())
+    }
+
+
+    #[test]
+    fn test_euler_do_step_iter_f64() {
+        let eu = Euler {};
+        let mut x = vec!(2.0, 3.0, 4.0, 5.0);
+        let mut dx = vec!(0.0, 0.0, 0.0, 0.0);
+        let dt = 0.1;
+        let t = 2.0;
+        let p = 4.0;
+        eu.do_step_iter(&rhs_vec, &mut x, &mut dx, &t, &dt, &p).unwrap();
+    }
+
+    #[test]
+    fn test_euler_do_step_iter_f32() {
+        let eu = Euler {};
+        let mut x = vec!(2.0f32, 3.0f32, 4.0f32, 5.0f32);
+        let mut dx = vec!(0.0f32, 0.0f32, 0.0f32, 0.0f32);
+        let dt = 0.1f32;
+        let t = 2.0f32;
+        let p = 4.0f32;
+        eu.do_step_iter(&rhs_vec, &mut x, &mut dx, &t, &dt, &p).unwrap();
+    }
+
+    #[test]
+    fn test_euler_do_step_add_f64() {
+        let eu = Euler {};
+        let mut x = 2.0;
+        let mut dx = 0.0;
+        let dt = 0.1;
+        let t = 2.0;
+        let p = 4.0;
+        eu.do_step_add(&rhs_add, &mut x, &mut dx, &t, &dt, &p).unwrap();
+    }
+
+    #[test]
+    fn test_euler_do_step_add_f32() {
+        let eu = Euler {};
+        let mut x = 2.0f32;
+        let mut dx = 0.0f32;
+        let dt = 0.1f32;
+        let t = 2.0f32;
+        let p = 4.0f32;
+        eu.do_step_add(&rhs_add, &mut x, &mut dx, &t, &dt, &p).unwrap();
+    }
+
+    fn rhs_bad_add<I, F>(_x: &I, _dx: &mut I, _t: &F, _p: &F) -> Result<(), CalcError>
+    where
+        I: AddAssign + Copy + Mul<F,Output=I> + Mul<F,Output=I> + std::ops::Neg<Output=I> + std::panic::RefUnwindSafe,
+        F: Add<F,Output=F> + Sub<F,Output=F> + Mul<F,Output=F> + Div<F,Output=F> + AddAssign + SubAssign + Neg + Copy + From<i8> + std::panic::RefUnwindSafe + Mul<I,Output=I>,
+    {
+        panic!("Purposefully panic to test Solver!");
+    }
+
+    #[test]
+    fn test_euler_catch_calc_panic_add_f64() {
+        let eu = Euler {};
+        let mut x = 1.2943859;
+        let mut dx = 0.0;
+        let dt = 0.323987;
+        let t = 5.23423987;
+        let p = 345.394857;
+        match eu.do_step_add(&rhs_bad_add, &mut x, &mut dx, &t, &dt, &p) {
+            Ok(()) => panic!("We did not catch the error"),
+            Err(CalcError) => (),
+        };
+    }
+
+    #[test]
+    fn test_euler_catch_calc_panic_add_f32() {
+        let eu = Euler {};
+        let mut x = 1.2943859f32;
+        let mut dx = 0.0f32;
+        let dt = 0.323987f32;
+        let t = 5.23423987f32;
+        let p = 345.394857f32;
+        match eu.do_step_add(&rhs_bad_add, &mut x, &mut dx, &t, &dt, &p) {
+            Ok(()) => panic!("We did not catch the error"),
+            Err(CalcError) => (),
+        };
+    }
+
+    fn rhs_bad_iter<F>(x: &Vec<F>, dx: &mut Vec<F>, _t: &F, p: &F) -> Result<(), CalcError>
+    where
+        F: Copy + Add<Output=F> + Add<F,Output=F> + AddAssign + Mul<F,Output=F> + std::ops::Neg<Output=F> + Div<F,Output=F> + From<i8> + std::panic::RefUnwindSafe,
+    {   
+        let l  =  x.len();
+        let dl = dx.len();
+        dx[dl+1] = - *p * x[l+1];
+        Ok(())
+    }
+
+    #[test]
+    fn test_euler_catch_calc_panic_iter_f32() {
+        let eu = Euler {};
+        let mut x = vec!(1.2943859f32, 239489.2394879f32, 11.987908234f32);
+        let mut dx = vec!(0.0f32, 0.0f32, 0.0f32);
+        let dt = 0.323987f32;
+        let t = 5.23423987f32;
+        let p = 345.394857f32;
+        match eu.do_step_iter(&rhs_bad_iter, &mut x, &mut dx, &t, &dt, &p) {
+            Ok(()) => panic!("We did not catch the error"),
+            Err(CalcError) => (),
+        };
+    }
+
+    #[test]
+    fn test_euler_catch_calc_panic_iter_f64() {
+        let eu = Euler {};
+        let mut x = vec!(1.2943859, 239489.2394879, 11.987908234);
+        let mut dx = vec!(0.0, 0.0, 0.0);
+        let dt = 0.323987;
+        let t = 5.23423987;
+        let p = 345.394857;
+        match eu.do_step_iter(&rhs_bad_iter, &mut x, &mut dx, &t, &dt, &p) {
+            Ok(()) => panic!("We did not catch the error"),
+            Err(CalcError) => (),
+        };
+    }
+}
