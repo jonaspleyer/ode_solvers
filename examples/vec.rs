76f70aec1dcbe414e4218d93d635631d5f92b0ca
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-09-01 17:02:46 +0200

    examples: also integrate with runge-kutta 4 solver

--- examples/vec.rs
+++ examples/vec.rs
@@ -4,7 +4,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use ode_integrate::concepts::errors::CalcError;
-use ode_integrate::solvers::fixed_step::{Euler};
+use ode_integrate::solvers::fixed_step::{RK4,Euler};
 use ode_integrate::concepts::steppers::Stepper;
 
 
@@ -33,7 +33,8 @@ fn rhs_scalar(y: &f64, dy: &mut f64, _t: &f64, p: &f64) -> Result<(), CalcError>
 
 fn main() {
     let mut x = 2.0;
-    let mut dx = 0.0;
+
+    let mut x2 = 2.0;
 
     let mut y = vec![1.0 ,2.0, 3.0];
 
@@ -41,16 +42,22 @@ fn main() {
 
     let p = 2.0;
 
-    let dt = 0.1;
+    let dt = 0.05;
     let mut t = 0.0;
     let tmax = 2.0;
 
+    let mut rk4_x = RK4::from((&x, &t, &dt, &p));
+    let mut rk4_y = RK4::from((&y, &t, &dt, &p));
+    let mut rk4_z = RK4::from((&z, &t, &dt, &p));
 
     let mut eu = Euler::from((&x2, &t, &dt, &p));
 
     while t<tmax {
         println!("t={:6.4} x={:6.4} x2={:6.4} y=[{:6.4} {:6.4} {:6.4}] z=[{:6.4} {:6.4} {:6.4}]", t, x, x2, y[0], y[1], y[2], z[0], z[1], z[2]);
+        rk4_x.do_step_add(&rhs_scalar, &mut x, &t, &dt, &p).unwrap();
         eu.do_step_add(&rhs_scalar, &mut x2, &t, &dt, &p).unwrap();
+        rk4_y.do_step_iter(&rhs_vec, &mut y, &t, &dt, &p).unwrap();
+        rk4_z.do_step_iter(&rhs_arr, &mut z, &t, &dt, &p).unwrap();
         t += dt;
     }
     println!("t={:6.4} x={:6.4} x2={:6.4} y=[{:6.4} {:6.4} {:6.4}] z=[{:6.4} {:6.4} {:6.4}]", t, x, x2, y[0], y[1], y[2], z[0], z[1], z[2]);
