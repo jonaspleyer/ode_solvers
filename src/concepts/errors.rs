a6c3c796de77339b1edd9b6a5ec04216d7180f25
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-09-06 16:32:56 +0200

    errors: add SolvingError implement more Traits
    - The SolvingError is meant to be thrown when the solving (instead of
      the Calculation of the RHS of the ODE) fails.
    - We can obtain a solving error from a calculation error.
    - We can obtain both errors from a text message.

--- src/concepts/errors.rs
+++ src/concepts/errors.rs
@@ -6,15 +6,78 @@
 use std::error::Error;
 use std::fmt;
 
-#[derive(Debug)]
-pub struct CalcError;
-
+/// # Error while calculating RHS of ODE
+/// When the evaluation of the RHS of the ODE
+/// \begin{equation}
+///     f(y, t, p) = \dots
+/// \end{equation}
+/// encounters an error, this one should be used.
+#[derive(Debug,Clone)]
+pub struct CalcError {
+    message: String,
+}
 
 impl fmt::Display for CalcError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Calculation Error occured!")
+        write!(f, "Calculation Error occured due to: {}", self.message)
+    }
+}
+
+impl Error for CalcError {}
+
+impl<'a> From<&'a str> for CalcError {
+    fn from(string: &'a str) -> Self {
+        CalcError {
+            message: string.to_owned(),
+        }
+    }
+}
+
+impl From<String> for CalcError {
+    fn from(string: String) -> Self {
+        CalcError {
+            message: string,
+        }
     }
 }
 
 
-impl Error for CalcError {}
+/// # Error during solving process
+/// When the solving process, which depends on the solver used does not produce
+/// a result, this error should be used.
+#[derive(Debug,Clone)]
+pub struct SolvingError {
+    message: String,
+}
+
+impl fmt::Display for SolvingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Solving Error occurred due to: {}", self.message)
+    }
+}
+
+impl Error for SolvingError {}
+
+impl From<CalcError> for SolvingError {
+    fn from(error: CalcError) -> Self {
+        SolvingError {
+            message: error.message,
+        }
+    }
+}
+
+impl<'a> From<&'a str> for SolvingError {
+    fn from(string: &'a str) -> Self {
+        SolvingError {
+            message: string.to_owned(),
+        }
+    }
+}
+
+impl From<String> for SolvingError {
+    fn from(string: String) -> Self {
+        SolvingError {
+            message: string,
+        }
+    }
+}
