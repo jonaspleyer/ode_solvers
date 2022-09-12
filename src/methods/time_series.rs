7b6d628d158fa10d81013679bd0aa59549f958fc
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2022-09-12 16:26:24 +0200

    methods: new methods for minimal stepsize

--- src/methods/time_series.rs
+++ src/methods/time_series.rs
@@ -157,3 +157,167 @@ where
     }
     Ok(y_res)
 }
+
+
+/// ## Example
+/// First we define the RHS of the ODE \\(f(y, t, p) = \dots\\).
+/// Then specify initial values \\(y_0\\), parameters \\(p\\), and time points \\(t_i\\).
+/// Afterwards integrate the ODE and display results.
+/// ```
+/// use ode_integrate::methods::time_series::*;
+/// use ode_integrate::prelude::*;
+/// 
+/// fn rhs_arr(y: &[f64; 3], dy: &mut [f64; 3], _t: &f64, p: &f64) -> Result<(), CalcError> {
+///     dy[0] = -p * y[0];
+///     dy[1] = -p * y[1];
+///     dy[2] = -p * y[2];
+///     Ok(())
+/// }
+/// 
+/// // Define initial values and parameters for the ODE
+/// let y0 = [1.0 ,2.0, 3.0];
+/// let p = 2.0;
+/// 
+/// // Define the time series on which to solve the ODE
+/// let mut t_series = Vec::<f64>::new();
+/// for n in 0..50 {
+///     t_series.push(n as f64 * 0.01);
+/// }
+/// 
+/// // Actually numerically integrate the ODE
+/// let res = solve_ode_time_series_minimal_step_iter(&y0, &t_series, &rhs_arr, &p, Rk4, &0.004);
+/// 
+/// // Check if solving was successfull and print if so
+/// match res {
+///     Ok(y_res) => {
+///         for (ti, yi) in t_series.iter().zip(y_res.iter()) {
+///             println!("t={:6.4}, y=[{:6.4} {:6.4} {:6.4}]", ti, yi[0], yi[1], yi[2]);
+///         }
+///     }
+///     Err(error) => {
+///         println!("An error occurred: {error}");
+///     }
+/// }
+/// ```
+pub fn solve_ode_time_series_minimal_step_iter<'a, I, F, P, E, V>
+(
+    y0: &I,
+    t_series: &V,
+    rhs: RHS<'a, I, F, P, E>,
+    p: &P,
+    solver_type: FixedStepSolvers,
+    dt: &F
+) -> Result<Vec<I>, SolvingError>
+where
+    for<'m>&'m mut I: IntoIterator<Item=&'m mut F>,
+    for<'m>&'m I: IntoIterator<Item=&'m F>,
+    I: Clone,
+    F: FloatLikeType + std::fmt::Debug,
+    P: Clone,
+    E: Error + Clone,
+    for<'m>&'m V: IntoIterator<Item=&'m F>,
+{
+    let t_initial = t_series.into_iter().next();
+    let t0 = match t_initial {
+        Some(t) => t,
+        None => return Err(SolvingError::from("Did not supply enough time steps.")),
+    };
+    let ode_def = OdeDefinition {
+        y0: y0.clone(),
+        t0: *t0,
+        func: rhs,
+    };
+    
+    let mut stepper = get_fixed_step_stepper(solver_type, ode_def);
+    let mut y = y0.clone();
+
+    // TODO In the future use the method: with_capacity(t_series.len())
+    // This is currently not possible since len() is a function inherent to std::Vec and not any trait.
+    let mut y_res = vec![y0.clone()];
+
+    let mut dtau: F;
+    let mut t_further = t_series.into_iter();
+    t_further.next();
+    for (t_i, t_j) in t_series.into_iter().zip(t_further) {
+        let mut t = *t_i;
+        while t < *t_j {
+            if *dt > *t_j - t {
+                dtau = *t_j - t;
+            } else {
+                dtau = *dt;
+            }
+            if dtau < F::from(0) {
+                return Err(SolvingError::from("Time steps need to be increasing"));
+            }
+            // Do step and save
+            match stepper.do_step_iter(&mut y, &t, &dt, p) {
+                Ok(()) => (),
+                Err(error) => return Err(SolvingError::from(format!("{error} {:?}",error))),
+            }
+            t += dtau;
+        }
+        y_res.push(y.clone());
+    }
+    Ok(y_res)
+}
+
+
+pub fn solve_ode_time_series_minimal_step_add<'a, I, F, P, E, V>
+(
+    y0: &I,
+    t_series: &V,
+    rhs: RHS<'a, I, F, P, E>,
+    p: &P,
+    solver_type: FixedStepSolvers,
+    dt: &F
+) -> Result<Vec<I>, SolvingError>
+where
+    I: MathVecLikeType<F>,
+    F: FloatLikeType + Mul<I,Output=I>,
+    P: Clone,
+    E: Error + Clone,
+    for<'m>&'m V: IntoIterator<Item=&'m F>,
+{
+    let t_initial = t_series.into_iter().next();
+    let t0 = match t_initial {
+        Some(t) => t,
+        None => return Err(SolvingError::from("Did not supply enough time steps.")),
+    };
+    let ode_def = OdeDefinition {
+        y0: y0.clone(),
+        t0: *t0,
+        func: rhs,
+    };
+    
+    let mut stepper = get_fixed_step_stepper(solver_type, ode_def);
+    let mut y = y0.clone();
+
+    // TODO In the future use the method: with_capacity(t_series.len())
+    // This is currently not possible since len() is a function inherent to std::Vec and not any trait.
+    let mut y_res = vec![y0.clone()];
+
+    let mut dtau: F;
+    let mut t_further = t_series.into_iter();
+    t_further.next();
+    for (t_i, t_j) in t_series.into_iter().zip(t_further) {
+        let mut t = *t_i;
+        while t < *t_j {
+            if *dt > *t_j - t {
+                dtau = *t_j - t;
+            } else {
+                dtau = *dt;
+            }
+            if dtau < F::from(0) {
+                return Err(SolvingError::from("Time steps need to be increasing"));
+            }
+            // Do step and save
+            match stepper.do_step_add(&mut y, &t, &dt, p) {
+                Ok(()) => (),
+                Err(error) => return Err(SolvingError::from(format!("{error} {:?}",error))),
+            }
+            t += dtau;
+        }
+        y_res.push(y.clone());
+    }
+    Ok(y_res)
+}
